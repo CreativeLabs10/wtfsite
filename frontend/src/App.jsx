06be4b88435924d17d76f68df37c54e1610e4985
PetrolioWeb <pter.pankov@yandex.ru>
2025-06-19 20:20:52 +0300

    Added base structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
+import React from 'react';  
+import { BrowserRouter, Routes, Route } from 'react-router-dom';  
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Homepage from './pages/Homepage'
 
 
-function App() {
- return (
-  <>
-  <h1>Hello, World!</h1>
-  
-  </>
- )
+
+function App() {  
+    return (
+      <>
+        <BrowserRouter>  
+            <Routes>  
+                <Route path="/login" element={<Login />} />  
+                <Route path="/register" element={<Register />} />  
+                <Route path="/" element={<Homepage />} />  
+            </Routes>  
+        </BrowserRouter>  
+      </> 
+    )
 }
-
 export default App

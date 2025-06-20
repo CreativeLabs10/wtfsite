9bd26128cc06e5c5bc97c879df6138318cb84547
CreativeLabs <163581316+CreativeLabs10@users.noreply.github.com>
2025-06-20 13:22:31 +0300

    homepage

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -3,6 +3,24 @@ function Homepage(){
   return (
     <>
     <h1>Homepage</h1>
+
+    <header>
+
+       <h1 class="logo">WTF Site</h1>
+
+        <nav class="nav">
+            <a href="main_page.html">Конвертеры</a>
+            <a href="service">Факт дня</a>
+            <a href="content">Контент</a>
+            <a href="about">О нас</a>
+        </nav>
+
+        <div class="log">
+            <a class="login" href="entry_with_userid">Войти</a>
+            <a class="signup" href="quick_register.html">Регистрация</a>
+        </div>
+
+    </header>
     </>
   )
 } 
